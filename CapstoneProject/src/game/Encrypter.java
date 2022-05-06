f26177c0c1f01a8b04e4cd11b8508ce960651f5d
Dhruv Lohani <dlohani334@student.fuhsd.org>
2022-05-06 15:26:38 -0700

    Added javadocs on top of all classes so it shows up in the javadocs

--- CapstoneProject/src/game/Encrypter.java
+++ CapstoneProject/src/game/Encrypter.java
@@ -1,11 +1,20 @@
 package game;
 
+
+
+/**
+ * would facilitate how the encryption codes works, and how it would be implemented in our game
+ * @author dhruv
+ *
+ */
 public class Encrypter {
 	
 	private static int level;
 	
 	private String original;
 	private String encrypted;
+	
+	
 	
 	public static void encrypt()
 	{
--- CapstoneProject/src/game/InfoBar.java
+++ CapstoneProject/src/game/InfoBar.java
@@ -2,6 +2,11 @@ package game;
 import items.*;
 import screen.*;
 
+
+/**
+ * @author Arindam
+ *
+ */
 public class InfoBar extends ScreenObject {
 	
 	// has access to main officer 
