72272bf2edb7d611f8135cc85ec63100f186e63c
Arindam Kulkarni <89165717+arindammm@users.noreply.github.com>
2022-05-11 10:19:54 -0700

    set up class with fields and constructor

--- CapstoneProject/src/characters/Antagonist.java
+++ CapstoneProject/src/characters/Antagonist.java
@@ -3,8 +3,14 @@ import processing.core.*;
 
 public class Antagonist extends Grinch {
 	
-	public Antagonist()
+	private int health;
+	private boolean isAlive;
+	private double proximity;
+	
+	public Antagonist(int x, double z)
 	{
-		
-	}
+		health = x;
+		isAlive = health > 0;
+		proximity = z;
+	} 
 }
