271a9ba54967a6d546eb144dbad11d64b0b1088a
Arindam Kulkarni <89165717+arindammm@users.noreply.github.com>
2022-05-11 11:35:21 -0700

    Update InfoBar.java

--- CapstoneProject/src/game/InfoBar.java
+++ CapstoneProject/src/game/InfoBar.java
@@ -11,22 +11,5 @@ public class InfoBar extends ScreenObject {
 	
 	// has access to main officer 
 	
-	public InfoBar()
-	{
-		super(0, 0, 100, 300);
-	}
 	
-	public void draw(PApplet marker)
-	{
-		marker.push();
-		
-		x = marker.width-w - 50;
-		y = 30;
-		
-		marker.rect((float)x, (float)y, (float)w, (float)h);
-		marker.fill(0, 0, 0);
-		marker.text("Info Bar", (float)(x + w/2), (float)(y + h/2));
-		
-		marker.pop();
-	}
 }
