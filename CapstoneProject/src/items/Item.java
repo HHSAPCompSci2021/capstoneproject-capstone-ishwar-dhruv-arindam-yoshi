25249723f562fae0f37cedaf4709cdc1393cc53c
Arindam Kulkarni <89165717+arindammm@users.noreply.github.com>
2022-05-12 17:54:07 -0700

    creating item class

--- CapstoneProject/src/items/Item.java
+++ CapstoneProject/src/items/Item.java
@@ -2,14 +2,12 @@ package items;
 import screen.ScreenObject;
 import processing.core.*;
 
-public abstract class Item extends ScreenObject {
-
-	public Item(int x, int y, int width, int height)
-	{
+public abstract class Item<E> extends ScreenObject {
+	public Item(int x, int y, int width, int height) {
 		super(x, y, width, height);
 	}
-	
-	public abstract Object use();
-	
-	public abstract void drawInfo(PApplet marker, int x, int y);
+	public abstract E use();
+
+	public abstract void drawInfo(PApplet marker, int x, int y); 
 }
+
