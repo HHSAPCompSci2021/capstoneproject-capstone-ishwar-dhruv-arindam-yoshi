c58af913168b9b8925975bf5c05a9873fcc4ae53
Dhruv Lohani <dlohani334@student.fuhsd.org>
2022-05-15 12:47:47 -0700

    Started trap

--- CapstoneProject/src/items/Trap.java
+++ CapstoneProject/src/items/Trap.java
@@ -1,5 +1,6 @@
 package items;
 import screen.ScreenObject;
+import characters.Officer;
 import game.HauntedMaze;
 import processing.core.*;
 
@@ -12,12 +13,28 @@ public class Trap extends Item {
 		super(x, y, width, height);
 	}
 	
-	public void use(HauntedMaze h)
+	public void use(HauntedMaze maze)
 	{
+		Officer o = maze.protagonist; 
+		o.changeHealth(-10);
 	}
 	
-	public void drawInfo(PApplet marker, double x, double y)
-	{
+	/**
+	 * Traps are not visible to the player, so in essense this method is purposeless. 
+	 * @param marker
+	 * @param x
+	 * @param y
+	 */
+	public void draw(PApplet marker, double x, double y) {
+		marker.push();	
+		marker.fill(0);
+		marker.image(null, (float)x, (float)y, (float)w, (float)h);
+		marker.pop();
+	}
+
+	@Override
+	public void drawInfo(PApplet marker, double x, double y) {
+		return; 
 		
 	}
 }
