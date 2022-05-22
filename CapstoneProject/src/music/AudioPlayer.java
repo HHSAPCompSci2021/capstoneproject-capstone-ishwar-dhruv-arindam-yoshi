5893c68ffdab7eb19a5a335498ba7e2474199abd
Arindam Kulkarni <89165717+arindammm@users.noreply.github.com>
2022-05-22 16:15:26 -0700

    finished adding the music for the program

--- CapstoneProject/src/music/AudioPlayer.java
+++ CapstoneProject/src/music/AudioPlayer.java
@@ -13,22 +13,68 @@ public class AudioPlayer {
 	public boolean hasMusicStarted = false; 
 	
 	public AudioPlayer(File file) {
-	
+		this.file = file; 
+		try {
+			audioStream = AudioSystem.getAudioInputStream(file);
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			clip = AudioSystem.getClip();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}  
+		try {
+			clip.open(audioStream);
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public AudioPlayer(String s) {
-	
+		File music = new File(s); 
+		this.file = music; 
+		try {
+			audioStream = AudioSystem.getAudioInputStream(file);
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			clip = AudioSystem.getClip();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}  
 	}
 	
 	public void play() {
-	
+		try {
+			clip.open(audioStream);
+			clip.start();
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void pause() {
-		
+		clip.stop();
 	}
 	
 	public void reset() {
-
+		clip.setMicrosecondPosition(0);
 	}
 }
