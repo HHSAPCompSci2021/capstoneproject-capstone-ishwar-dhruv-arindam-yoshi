467ef6be5e0474fe818177ff0fa75685c250da2d
Dhruv Lohani <dlohani334@student.fuhsd.org>
2022-05-24 03:26:56 -0700

    Updated javadocs

--- CapstoneProject/src/characters/Antagonist.java
+++ CapstoneProject/src/characters/Antagonist.java
@@ -8,6 +8,12 @@ public class Antagonist extends Grinch {
 	private double proximity;
 	private static final String IMG_PATH = "grinch.png";
 	
+	/**
+	 * Creates an object of the antagonist class
+	 * @param marker the PApplet in which the antagonist is on 
+	 * @param x the x coordinate of the antagonist
+	 * @param y the y coordinate of the antagonist 
+	 */
 	public Antagonist(PApplet marker, double x, double y) {
 		super(marker, x, y);
 	}
